aa586e8634d9a27a9e909e2e601affb5006518d7
Kevin Zheng <kevinzheng@Kevins-MacBook-Pro.local>
2021-02-21 19:12:33 -0800

    edit readme

--- README.md
+++ README.md
@@ -21,14 +21,13 @@
 - To install dependencies run `npm i`
 - To run the web app and api server together, run `npm run dev`
 - To run the web app separately, run `npm run start`
-  - This may take a minute or 2 to run
 - To run api server separately, (in another terminal) run `npm run server`
 
 ## Twitter-Screenname-Server API
 
 - Run the twitter-screenname-server by running `npm run server`
 - The server should be running on `http://localhost:4000`
-- Navigating to `http://localhost:4000/twitter/user/search?username=chicago` should return a large JSON response.
+- Navigating to `http://localhost:4000/twitter/user/search?username=cat` should return a large JSON response.
 - To use the api, you can make a request directly to `http://localhost:4000`.
 - If port 4000 is in use, feel free to update /twitter-screenname-server/server.js source code to use a different port.
 
--- twitter-screenname-server/server.js
+++ twitter-screenname-server/server.js
@@ -1,39 +1,40 @@
-const Twitter = require('twitter');
-const express = require('express');
+const Twitter = require("twitter");
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const PORT = 4000;
+
 //In Production Code, the consumer_key, consumer_secret,access_token_key and access_token_secret would not be shown for security issue.
 //Since this is a simple twitter clone, I am displaying here for convinence purpose for the readder
 const client = new Twitter({
-	consumer_key: 'Rc4rjFT7IgOVuH6g74znYVEtd',
-	consumer_secret: 'fW0uPs0zLL3fwVkPdwYPx09xajA1p6InoLB50H2MiA1JGOWADn',
-	access_token_key: '842866092888416256-VLBW5odiBC3U2pCEVwJ8RtRDUclYWjt',
-	access_token_secret: 'LkIENI6ryVU9dcICxrUNjb20uPQcd1oZhDFkfEtqFWrs7',
+  consumer_key: "Rc4rjFT7IgOVuH6g74znYVEtd",
+  consumer_secret: "fW0uPs0zLL3fwVkPdwYPx09xajA1p6InoLB50H2MiA1JGOWADn",
+  access_token_key: "842866092888416256-VLBW5odiBC3U2pCEVwJ8RtRDUclYWjt",
+  access_token_secret: "LkIENI6ryVU9dcICxrUNjb20uPQcd1oZhDFkfEtqFWrs7",
 });
 
 // CORS support
 app.use(cors());
-app.get('/', (req, res) => {
-	res.status(200).send({
-		data: 'Successful request',
-	});
+app.get("/", (req, res) => {
+  res.status(200).send({
+    data: "Successful request",
+  });
 });
 
-app.get('/twitter/user/search', (req, res) => {
-	const username = req.query.username;
+app.get("/twitter/user/search", (req, res) => {
+  const username = req.query.username;
 
-	client.get('/users/search', { q: username }, (error, users, response) => {
-		if (error) {
-			res.status(error.code).send({ error });
-		} else {
-			res.status(200).send({ users, response });
-		}
-	});
+  client.get("/users/search", { q: username }, (error, users, response) => {
+    if (error) {
+      res.status(error.code).send({ error });
+    } else {
+      res.status(200).send({ users, response });
+    }
+  });
 });
 
 app.listen(PORT, () => {
-	/* eslint-disable no-console */
-	console.log('listening on port ' + PORT + '...');
-	/* eslint-enable no-console */
+  /* eslint-disable no-console */
+  console.log("listening on port " + PORT + "...");
+  /* eslint-enable no-console */
 });
